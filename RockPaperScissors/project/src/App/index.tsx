bc173024f3e7ce5043092e030649b77f2d36b68b
will <willswordpath@gmail.com>
2021-09-10 15:48:00 +0800

    start: Rock Paper Scissors

---
+++ RockPaperScissors/project/src/App/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+export default React.memo(function () {
+    return <>
+        <h1>Hello React</h1>
+        <p>Some content presented</p>
+    </>
+})
---
+++ RockPaperScissors/project/src/main.tsx
@@ -0,0 +1,11 @@
+import './styles/root.css'
+import './styles/global.css'
+
+import * as React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+
+render(
+    <App></App>
+    , document.getElementById('root')
+)
